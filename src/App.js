ef6613cb023a8519fb6f7fc3a8ab69061d11e940
Ross Perkel <rossperkel@gmail.com>
2019-11-05 16:48:13 -0500

    created forecast container to hold all weather related data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js'
+import ForecastContainer from './ForecastContainer.js'
 
 class App extends React.Component {
   render(){
     return (
       <Header title={"Weather Near You"}/>
-      
+      <ForecastContainer />
   );
   }
 }
